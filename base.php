a0fa1f27398be4e989d2c0e016511606beda5337
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2020-10-30 10:41:03 -0300

    Create base.php

---
+++ base.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container">
+        <div class="logo">
+            <--barra logo-->
+        </div>
+        <div class="usuario">
+            <--entrar-->
+        </div>
+        <div class="titulo">
+            <--titulo-->
+        </div>
+        <div class="hamburger">
+            <--menu hamburger-->
+        </div>
+        <div class="footer">
+            <--footer-->
+        </div>
+    </div>
+</body>
+</html>
